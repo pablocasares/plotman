50d0fac9e2d7c17f146f2ef0c59b57e52c14b1ef
Pablo Casares <lito92vforce@gmail.com>
2021-05-01 03:20:21 +0200

    Update archive.py

--- src/plotman/archive.py
+++ src/plotman/archive.py
@@ -49,7 +49,7 @@ def compute_priority(phase, gb_free, n_plots):
 
 def get_archdir_freebytes(arch_cfg):
     archdir_freebytes = {}
-    df_cmd = ('ssh -p %s -i /home/chia/.ssh/id_rsa_for_df %s@%s df -aBK | grep " %s/"' %
+    df_cmd = ('ssh -p %s -i /home/chia/.ssh/id_rsa_for_df %s@%s df -aBK | grep " %s"' %
         (arch_cfg.ssh_port, arch_cfg.rsyncd_user, arch_cfg.rsyncd_host, arch_cfg.rsyncd_path) )
     print("Size available cmd: " + df_cmd)
     with subprocess.Popen(df_cmd, shell=True, stdout=subprocess.PIPE) as proc:
